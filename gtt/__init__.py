afd4a5c7355c0c713776b4b34a87be79b5d5cdc9
Andrew Blomenberg <andrewBlomen@gmail.com>
2022-02-27 15:11:42 -0700

    Starter package files

---
+++ gtt/__init__.py
@@ -0,0 +1,3 @@
+from gtt.gtt_display import GttDisplay
+
+__version__ = "0.0.1"
---
+++ gtt/gtt_display.py
@@ -0,0 +1,7 @@
+from serial import Serial
+
+
+class GttDisplay:
+    def __init__(self, port:str, x_size: int, y_size: int):
+        pass
+
